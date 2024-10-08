03993cc239f9a70d15a186dd3e303b8aa54127ef
Tushar <100490977+tusharnain4578@users.noreply.github.com>
2024-10-08 12:09:14 +0530

    Create redirect from root to admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
-
+use App\Http\Controllers\RedirectController;
 
 Route::prefix('admin')->name('admin.')->group(function () {
     require_once 'admin.php';
 });
+
+// I made separate redirect controller, beecause closures in routes don't get cached
+
+Route::get('/', [RedirectController::class, 'rootToAdminDashboard']);
